fc62b83d20f22aba785016ef083cb7b7897e9936
Marcelo <mnvargas312@gmail.com>
2024-11-05 16:49:52 -0500

    populate the Employee class with attributes, get, and set functions.

--- Employee.h
+++ Employee.h
@@ -3,6 +3,25 @@
 #include <string>
 
 Class Employee {
+public:
+    Employee(int id, const std::string &name, const std::string &department, double salary);
+    
+    int getID() const;
+
+    std::string getName() const;
+    void setName(const std::string &name);
+
+    std::string getDepartment() const;
+    void setDepartment(const std::string &department)
+
+    double getSalary() const;
+    void setSalary(double salary);
+
+private:
+    int id;
+    std::string name;
+    std::string department;
+    double salary;
 
 };
 
